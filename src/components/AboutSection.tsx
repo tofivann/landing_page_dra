1f4973f899c0764e652b0b45aa9b586b803bc1c8
ivan <ivanzerpaz.22@gmail.com>
2026-02-03 22:03:55 -0400

    cambio a la  seccion de about

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -15,79 +15,53 @@ import imagen05 from "../imagenes/imagen05.png";
 import imagen06 from "../imagenes/imagen06.png";
 export function AboutSection() {
   return (
-    <Box id="sobre-mi" m="lg">
-      <Box p="sm">
-        <Container>
-          <Title order={1} className={classes.tituloSubrayado}>
-            Sobre Mi
-          </Title>
-        </Container>
-      </Box>
+    
+    <Box id="sobre-mi"  py="xl"> 
+      <Container>
+     
+        <Title order={1} className={classes.tituloSubrayado} mb="lg">
+          Sobre Mi
+        </Title>
+   
+        <Paper radius="md" withBorder p="xl" className={classes.inicioBox} >
+          <Group justify="center" align="center" gap="xl">
+            <Image
+              h={320}
+              w={220}
+              radius="xl"
+              src={imagen03}
+              alt="Foto Profesional"
+            />
+            
+           
+            <Text ta="justify" maw={350}>
+              Soy la Dra. Ivonne Sanchez, medica especialista en Ginecologia
+              y Obstetricia comprometida con el bienestar integral de mis
+              pacientes. Mi enfoque combina experiencia profesional,
+              tecnologia de vanguardia y un trato humano y personalizado.
+              <br /> <br />
+              Creo firmemente que cada mujer merece atencion medica de calidad
+              en un ambiente de confianza y respeto.
+            </Text>
 
-      <Box p="sm">
-        <Container size={"xl"}>
-          <Paper radius="md" withBorder className={classes.inicioBox}>
-            <Group p="xl" justify="center" align="center">
-              <Image
-                h={320}
-                w={220}
-                radius="xl"
-                src={imagen03}
-                alt="Foto Profesional"
-              />
-              <Text ta="justify">
-                Soy la Dra. Ivonne Sanchez , medica especialista en Ginecologia
-                y Obstetricia comprometida con el bienestar integral de mis
-                pacientes. mi enfoque combina experiencia profesional ,
-                tecnologia de vanguardia y un trato humano y personalizado.
-                <br />
-                Creo firmemetne que cada mujer merece atencion medica de calidad
-                en un ambiente de confianza y respeto. Mi mision es acompañarte
-                en cada etapa de tu vida con el mejor cuidado posible.
-              </Text>
-              <Carousel
-                slideSize="70%"
-                height={360}
-                w={300}
-                slideGap="md"
-                withIndicators
-              >
-                <Carousel.Slide>
-                  <Image
-                    src={imagen05}
-                    alt="Consultorio 1"
-                    h={360}
-                    w="100%"
-                    fit="cover"
-                    radius="md"
-                  />
-                </Carousel.Slide>
-
-                <Carousel.Slide>
-                  <Image
-                    src={imagen06}
-                    alt="Consultorio 2"
-                    h={360}
-                    w="100%"
-                    fit="cover"
-                    radius="md"
-                  />
-                </Carousel.Slide>
-
-                <Carousel.Slide>
-                  <Image
-                    src={imagen05}
-                    h={360}
-                    w="100%"
-                    fit="cover"
-                    radius="md"
-                  />
-                </Carousel.Slide>
-              </Carousel>
-            </Group>
-          </Paper>
-        </Container>
-      </Box>
+            <Carousel
+              slideSize="100%"
+              height={360}
+             
+              w={{ base: "100%", sm: 350 }} 
+              slideGap="md"
+              withIndicators
+            >
+              <Carousel.Slide>
+                <Image src={imagen05} alt="Consultorio 1" h={360} fit="cover" radius="md" />
+              </Carousel.Slide>
+              <Carousel.Slide>
+                <Image src={imagen06} alt="Consultorio 2" h={360} fit="cover" radius="md" />
+              </Carousel.Slide>
+            </Carousel>
+          </Group>
+        </Paper>
+      </Container>
     </Box>
   );
 }
